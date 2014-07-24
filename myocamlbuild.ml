4df43774551ab050484cbe4f40a548f44c18d9ca
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-07-24 20:23:26 +0100

    Hack to get correct link order for tests with ocamlbuild.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,6 +5,9 @@ let () =
     Ocamlbuild_js_of_ocaml.dispatcher d;
     match d with
     | After_rules ->
+        (* FIXME: hack to get the correct link order. *)
+        flag ["basefirst"; "byte"; "link"] (A "src/useri_base.cma");
+        flag ["basefirst"; "native"; "link"] (A "src/useri_base.cmxa");
         flag ["js_of_ocaml"; "weak_js"] (A "+weak.js");
         ocaml_lib ~dir:"src/tsdl" "src/tsdl/useri_tsdl";
         ocaml_lib ~dir:"src/jsoo" "src/jsoo/useri_jsoo";
