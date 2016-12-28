a62cf51db6eacf5e5f7a30eec91cfecb6a13d53d
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-12-28 23:11:10 +0100

    jsoo builds: add weak the jsoo way.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,7 +5,6 @@ let () =
     Ocamlbuild_js_of_ocaml.dispatcher d;
     match d with
     | After_rules ->
-        flag ["js_of_ocaml"; "weak_js"] (A "+weak.js");
         ocaml_lib ~tag_name:"base" "src/useri_base";
         ocaml_lib ~tag_name:"jsoo" "src-jsoo/useri_jsoo";
         ocaml_lib ~tag_name:"tsdl" "src-tsdl/useri_tsdl";
